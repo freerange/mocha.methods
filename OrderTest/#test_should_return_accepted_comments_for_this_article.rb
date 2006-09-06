a79e8b210f1ae29e3f6c78346a4418441f27d939
James Mead <james@floehopper.org>
2006-09-06 06:18:17 +0000

    Remove last traces of auto_mocha.

--- OrderTest/#test_should_return_accepted_comments_for_this_article.rb
+++
@@ -1,8 +0,0 @@
-def test_should_return_accepted_comments_for_this_article
-  unaccepted_comment = stub(:accepted? => false)
-  accepted_comment = stub(:accepted? => true)
-  comments = [unaccepted_comment, accepted_comment]
-  Comment.stubs(:find_all_by_article_id).returns(comments)
-  article = Article.new
-  assert_equal [accepted_comment], article.accepted_comments
-end
