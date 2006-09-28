caf83052575e949b2ae5cf5c6da56d3aebdeb350
Paul Battley <pbattley@gmail.com>
2006-09-28 15:00:19 +0000

    Bugfix: attempting to stub a method with = in its name would cause a parse error.

--- Stubba/ClassMethod/#hidden_method.rb
+++ Stubba/ClassMethod/#hidden_method.rb
@@ -1,3 +1,7 @@
 def hidden_method
-  "__stubba__#{method.to_s.sub('!', '_exclamation_mark').sub('?', '_question_mark')}__stubba__"
+  "__stubba__#{
+    method.to_s.sub('!', '_exclamation_mark').
+                sub('?', '_question_mark').
+                sub('=', '_equals_sign')
+  }__stubba__"
 end  
