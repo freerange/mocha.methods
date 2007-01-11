cf0425397c8ea5e564469dacee9ca756726849f8
James Mead <james@floehopper.org>
2007-01-11 17:26:31 +0000

    Allow mocking and stubbing of methods defined on Object including 'expects' and 'stubs'.

--- Mocha/MockMethods/#expects.rb
+++ Mocha/MockMethods/#expects.rb
@@ -2,6 +2,7 @@ def expects(method_names, backtrace = nil)
   method_names = method_names.is_a?(Hash) ? method_names : { method_names => nil }
   method_names.each do |method_name, return_value|
     expectations << Expectation.new(self, method_name, backtrace).returns(return_value)
+    self.__metaclass__.send(:undef_method, method_name) if self.__metaclass__.method_defined?(method_name)
   end
   expectations.last
 end
