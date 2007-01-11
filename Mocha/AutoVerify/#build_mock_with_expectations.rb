33a79095c392c4e1a665fe4ce28635d577206d9c
James Mead <james@floehopper.org>
2007-01-11 15:50:34 +0000

    Use Object#__send__ instead of Object#send so that Object#send can be mocked.

--- Mocha/AutoVerify/#build_mock_with_expectations.rb
+++ Mocha/AutoVerify/#build_mock_with_expectations.rb
@@ -3,7 +3,7 @@ def build_mock_with_expectations(expectation_type = :expects, expectations = {},
   expectation_type = :stubs if expectation_type == :stub_everything
   mock = Mocha::Mock.new(stub_everything, name)
   expectations.each do |method, result|
-    mock.send(expectation_type, method).returns(result)
+    mock.__send__(expectation_type, method).returns(result)
   end
   mocks << mock
   mock
