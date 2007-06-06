e6fcfb3cb3464a4f5dd4e6dce959a7947ee41fd7
James Mead <james@floehopper.org>
2007-06-06 16:55:56 +0000

    Fix so that it is possible for a stubbed method to raise an Interrupt exception without a message in Ruby 1.8.6

--- Mocha/ExceptionRaiser/#evaluate.rb
+++ Mocha/ExceptionRaiser/#evaluate.rb
@@ -1,7 +1,5 @@
 def evaluate
-  if @message then
-    raise @exception, @message
-  else
-    raise @exception
-  end
+  raise @exception, @exception.to_s if @exception == Interrupt
+  raise @exception, @message if @message
+  raise @exception
 end
