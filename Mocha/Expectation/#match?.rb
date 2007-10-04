abde9736a160257cb6c4a2bb16b09cd009430693
James Mead <james@floehopper.org>
2007-10-04 21:03:17 +0000

    Simplify Expectation#match logic.

--- Mocha/Expectation/#match?.rb
+++ Mocha/Expectation/#match?.rb
@@ -1,4 +1,3 @@
 def match?(method_name, *arguments)
-  return false unless @method_name == method_name
-  return @parameters.match?(arguments)
+  (@method_name == method_name) && @parameters.match?(arguments)
 end
