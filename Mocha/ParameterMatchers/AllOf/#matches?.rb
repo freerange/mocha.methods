5f79fd105bb0910f4afb1c154925ece10878d1ae
James Mead <james@floehopper.org>
2007-11-15 18:29:52 +0000

    Fixed bug in any_of and all_of parameter matchers.

--- Mocha/ParameterMatchers/AllOf/#matches?.rb
+++ Mocha/ParameterMatchers/AllOf/#matches?.rb
@@ -1,4 +1,4 @@
 def matches?(available_parameters)
   parameter = available_parameters.shift
-  @matchers.all? { |matcher| matcher == parameter }
+  @matchers.all? { |matcher| matcher.matches?([parameter]) }
 end
