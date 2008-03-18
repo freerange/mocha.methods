8130dcc7ea397edb94a1dca15d437be8347edca5
James Mead <james@floehopper.org>
2008-03-18 22:37:52 +0000

    Fix bug #18914 - Classes that override '==' are likely to experience mocking 'failures' (where the original method is called instead of the mocked method). Use #equal? instead of #== or #eql? on stubbee to determine whether ClassMethod instances are equivalent.

--- Mocha/ClassMethod/#eql?.rb
+++ Mocha/ClassMethod/#eql?.rb
@@ -1,4 +1,4 @@
 def eql?(other)
   return false unless (other.class == self.class)
-  (stubbee == other.stubbee) and (method == other.method)
+  (stubbee.equal?(other.stubbee)) and (method == other.method)
 end
