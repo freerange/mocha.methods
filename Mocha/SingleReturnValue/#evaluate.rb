056dd850039e46ac1888a8082d9a708db52bb21b
James Mead <james@floehopper.org>
2008-05-05 11:23:36 +0000

    Removed previously deprecated usage of Expectation#returns with a Proc.

--- Mocha/SingleReturnValue/#evaluate.rb
+++ Mocha/SingleReturnValue/#evaluate.rb
@@ -1,9 +1,3 @@
 def evaluate
-  if @value.__is_a__(Proc) then
-    message = 'use of Expectation#returns with instance of Proc - see Expectation#returns RDoc for alternatives'
-    Deprecation.warning(message)
-    @value.call
-  else
-    @value
-  end
+  @value
 end
