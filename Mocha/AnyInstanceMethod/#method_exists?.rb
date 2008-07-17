da7186eeb4d6d5a3433b9f1949ed35fb4839fc2c
James Mead <james@floehopper.org>
2008-07-17 15:16:50 +0000

    Now that ClassMethod#method is a string in Ruby 1.8 and a symbol in Ruby 1.9 we can simplify the implementation of all of the #method_exists? methods. These changes are also part of the speed improvements suggested by Chris McGrath.

--- Mocha/AnyInstanceMethod/#method_exists?.rb
+++ Mocha/AnyInstanceMethod/#method_exists?.rb
@@ -3,5 +3,5 @@ def method_exists?(method)
   existing_methods += stubbee.public_instance_methods(false)
   existing_methods += stubbee.protected_instance_methods(false)
   existing_methods += stubbee.private_instance_methods(false)
-  existing_methods.any? { |m| m.to_s == method.to_s }
+  existing_methods.include?(method)
 end
--- Mocha/InstanceMethod/#method_exists?.rb
+++ Mocha/InstanceMethod/#method_exists?.rb
@@ -3,5 +3,5 @@ def method_exists?(method)
   existing_methods += stubbee.public_methods(false)
   existing_methods += stubbee.protected_methods(false)
   existing_methods += stubbee.private_methods(false)
-  existing_methods.any? { |m| m.to_s == method.to_s }
+  existing_methods.include?(method)
 end
