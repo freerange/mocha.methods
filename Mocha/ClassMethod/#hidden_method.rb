c22a409cf83a5cf89063dfe3678ffe2fff73806f
James Mead <james@floehopper.org>
2008-07-17 15:00:44 +0000

    ClassMethod#method is normally a symbol, so it makes more sense if #hidden_method is also a symbol rather than a string.

--- Mocha/ClassMethod/#hidden_method.rb
+++ Mocha/ClassMethod/#hidden_method.rb
@@ -4,5 +4,5 @@ def hidden_method
   else
     method_name = method.to_s.gsub(/\W/) { |s| "_substituted_character_#{s.ord}_" }
   end
-  "__stubba__#{method_name}__stubba__"
+  "__stubba__#{method_name}__stubba__".to_sym
 end  
