6835a7b6655ceb307493f9863e67b18f570d34a9
jamesmead <jamesmead@c3f02bb1-6518-0410-a9f4-ca7cd5e7eb38>
2008-11-03 08:08:26 +0000

    Patch [#22630] Fix for a bug in running Rails tests with Ruby 1.8.7. Array#flatten was being called which in turn was checking whether each element responded to #to_ary. This check was using the two parameter version of #respond_to?, but Mock was only defining a one parameter version.

--- Mocha/Mock/#respond_to?.rb
+++ Mocha/Mock/#respond_to?.rb
@@ -1,6 +1,10 @@
-def respond_to?(symbol)
+def respond_to?(symbol, include_private = false)
   if @responder then
-    @responder.respond_to?(symbol)
+    if @responder.method(:respond_to?).arity > 1
+      @responder.respond_to?(symbol, include_private)
+    else
+      @responder.respond_to?(symbol)
+    end
   else
     @expectations.matches_method?(symbol)
   end
