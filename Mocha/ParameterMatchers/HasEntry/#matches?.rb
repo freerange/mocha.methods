4b650401451826a973a7405fb624d7d328fb829f
Taylor Barstow <taylorbarstow@gmail.com>
2009-03-03 07:14:11 +0800

    fix exception raised by HasEntry#matches? if first param is not a hash
    Signed-off-by: James Mead <james@floehopper.org>

--- Mocha/ParameterMatchers/HasEntry/#matches?.rb
+++ Mocha/ParameterMatchers/HasEntry/#matches?.rb
@@ -1,5 +1,6 @@
 def matches?(available_parameters)
   parameter = available_parameters.shift
+  return false unless parameter.respond_to?(:keys)
   matching_keys = parameter.keys.select { |key| @key.to_matcher.matches?([key]) }
   matching_keys.any? { |key| @value.to_matcher.matches?([parameter[key]]) }
 end
