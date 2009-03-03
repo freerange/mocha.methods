79d06513a62660a9cd25411b2ec2e8ee67ffb9e7
James Mead <james@floehopper.org>
2009-03-03 12:39:59 +0000

    Made commit 2df2193c by taylorbarstow a little more general.

--- Mocha/ParameterMatchers/HasEntry/#matches?.rb
+++ Mocha/ParameterMatchers/HasEntry/#matches?.rb
@@ -1,6 +1,6 @@
 def matches?(available_parameters)
   parameter = available_parameters.shift
-  return false unless parameter.respond_to?(:keys)
+  return false unless parameter.respond_to?(:keys) && parameter.respond_to?(:[])
   matching_keys = parameter.keys.select { |key| @key.to_matcher.matches?([key]) }
   matching_keys.any? { |key| @value.to_matcher.matches?([parameter[key]]) }
 end
