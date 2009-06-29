262d980cc44ae8e511592c91dffbd5b67b03b718
James Mead <james@floehopper.org>
2009-06-29 16:00:26 +0100

    Merge branch 'master' of git@github.com:floehopper/mocha

--- Mocha/ClassMethod/#define_new_method.rb
+++ Mocha/ClassMethod/#define_new_method.rb
@@ -1,3 +1,7 @@
 def define_new_method
-  stubbee.__metaclass__.class_eval("def #{method}(*args, &block); mocha.method_missing(:#{method}, *args, &block); end", __FILE__, __LINE__)
+  stubbee.__metaclass__.class_eval(%{
+    def #{method}(*args, &block)
+      mocha.method_missing(:#{method}, *args, &block)
+    end
+  }, __FILE__, __LINE__)
 end
