04a9fa81e1b02a695e5ed14a6f095eed9a68fedb
Sander Hartlage <sander@outside.in>
2009-07-09 00:00:04 +0800

    Now the has_key matcher will not raise an error on empty arguments or arguments that don't respond to #keys, nor will it match
    Signed-off-by: James Mead <james@floehopper.org>

--- Mocha/ParameterMatchers/HasKey/#matches?.rb
+++ Mocha/ParameterMatchers/HasKey/#matches?.rb
@@ -1,4 +1,5 @@
 def matches?(available_parameters)
   parameter = available_parameters.shift
+  return false unless parameter.respond_to?(:keys)
   parameter.keys.any? { |key| @key.to_matcher.matches?([key]) }
 end
