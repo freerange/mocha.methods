c7b0a4b57901f4c478674ee5c30db40ed08b0600
Sander Hartlage <sander@outside.in>
2009-07-09 00:01:18 +0800

    Now the has_value matcher won't match or raise error on empty arguments or arguments that don't respond to #values
    Signed-off-by: James Mead <james@floehopper.org>

--- Mocha/ParameterMatchers/HasValue/#matches?.rb
+++ Mocha/ParameterMatchers/HasValue/#matches?.rb
@@ -1,4 +1,5 @@
 def matches?(available_parameters)
   parameter = available_parameters.shift
+  return false unless parameter.respond_to?(:values)
   parameter.values.any? { |value| @value.to_matcher.matches?([value]) }
 end
