53b3364ea181f068c20360a38ba81680411447c8
James Mead <james@floehopper.org>
2010-11-30 09:24:32 +0000

    Include unexpected invocation and change the language slightly to make the failure message less confusing.
    See http://floehopper.lighthouseapp.com/projects/22289/tickets/52-never-expectation-failed-twisted-message

--- Mocha/Mock/#method_missing.rb
+++ Mocha/Mock/#method_missing.rb
@@ -6,6 +6,7 @@ def method_missing(symbol, *arguments, &block)
     matching_expectation_allowing_invocation.invoke(&block)
   else
     if (matching_expectation = @expectations.match(symbol, *arguments)) || (!matching_expectation && !@everything_stubbed)
+      matching_expectation.invoke(&block) if matching_expectation
       message = UnexpectedInvocation.new(self, symbol, *arguments).to_s
       message << Mockery.instance.mocha_inspect
       raise ExpectationError.new(message, caller)
