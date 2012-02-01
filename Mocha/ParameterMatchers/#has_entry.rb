f316f126ae7b1ff583f839738f6a67192d77d32e
James Mead <james@floehopper.org>
2012-02-01 12:41:14 +0000

    Fix for #45.
    Raise ArgumentError if Mocha::ParameterMatchers#has_entry is given
    Hash with wrong number of entries.

--- Mocha/ParameterMatchers/#has_entry.rb
+++ Mocha/ParameterMatchers/#has_entry.rb
@@ -1,5 +1,14 @@
 def has_entry(*options)
   key, value = options.shift, options.shift
-  key, value = key.to_a[0][0..1] if key.is_a?(Hash)
+  if key.is_a?(Hash)
+    case key.length
+    when 0
+      raise ArgumentError.new("Argument has no entries.")
+    when 1
+      key, value = key.to_a.flatten
+    else
+      raise ArgumentError.new("Argument has multiple entries. Use Mocha::ParameterMatchers#has_entries instead.")
+    end
+  end
   HasEntry.new(key, value)
 end
