efc5d8c134be447c74f8a4089b82c0807ecb3616
James Mead <james@floehopper.org>
2012-04-12 18:41:55 +0100

    ClassMethod#hidden_method is no longer needed.
    This is because we no longer use alias_method to hide the original
    method; instead we store it in an instance variable and re-define it if
    appropriate.

--- Mocha/ClassMethod/#hidden_method.rb
+++
@@ -1,9 +0,0 @@
-def hidden_method
-  if RUBY_VERSION < '1.9'
-    method_name = method.to_s.gsub(/\W/) { |s| "_substituted_character_#{s[0]}_" }
-  else
-    method_name = method.to_s.gsub(/\W/) { |s| "_substituted_character_#{s.ord}_" }
-  end
-  hidden_method = "__stubba__#{method_name}__stubba__"
-  RUBY_VERSION < '1.9' ? hidden_method.to_s : hidden_method.to_sym
-end
