c76db0e8d6909cb5191fdb4310f57403663d2823
James Mead <james@floehopper.org>
2012-07-04 15:50:59 +0100

    Rename method to make it less confusing.
    I think this is a carry over from an old name for the library.

--- Mocha/Integration/.mini_test_testcase_defined?.rb
+++ Mocha/Integration/.mini_test_testcase_defined?.rb
@@ -1,3 +1,3 @@
-def mini_unit_testcase_defined?
+def mini_test_testcase_defined?
   defined?(MiniTest::Unit::TestCase)
 end
--- Mocha/Integration/.monkey_patches.rb
+++ Mocha/Integration/.monkey_patches.rb
@@ -3,7 +3,7 @@ def monkey_patches
   if test_unit_testcase_defined? && !test_unit_testcase_inherits_from_miniunit_testcase?
     patches << 'mocha/integration/test_unit'
   end
-  if mini_unit_testcase_defined?
+  if mini_test_testcase_defined?
     patches << 'mocha/integration/mini_test'
   end
   patches
