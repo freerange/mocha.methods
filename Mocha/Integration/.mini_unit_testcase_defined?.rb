11f1f76cfad8cb112b00a77402ce1891d5c0075c
James Mead <james@floehopper.org>
2012-07-04 15:24:36 +0100

    No need to check `defined?` for outer modules.
    As suggested in [1].
    
    [1] https://github.com/freerange/mocha/pull/40#issuecomment-3264438

--- Mocha/Integration/.mini_unit_testcase_defined?.rb
+++ Mocha/Integration/.mini_unit_testcase_defined?.rb
@@ -1,3 +1,3 @@
 def mini_unit_testcase_defined?
-  defined?(MiniTest) && defined?(MiniTest::Unit) && defined?(MiniTest::Unit::TestCase)
+  defined?(MiniTest::Unit::TestCase)
 end
--- Mocha/Integration/.test_unit_testcase_defined?.rb
+++ Mocha/Integration/.test_unit_testcase_defined?.rb
@@ -1,3 +1,3 @@
 def test_unit_testcase_defined?
-  defined?(Test) && defined?(Test::Unit) && defined?(Test::Unit::TestCase)
+  defined?(Test::Unit::TestCase)
 end
