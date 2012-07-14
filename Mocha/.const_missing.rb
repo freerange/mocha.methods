b2d27d88739d8fcf3ca62c71573d486eb24df93c
James Mead <james@floehopper.org>
2012-07-14 18:33:27 +0100

    Remove deprecated access to `Mocha::Standalone`.
    The use of `Mocha::Standalone` has been deprecated for years and should
    be safe to remove. Use `Mocha::API` instead.

--- Mocha/.const_missing.rb
+++
@@ -1,6 +0,0 @@
-def self.const_missing(name)
-  return super unless name == :Standalone
-  require 'mocha/deprecation'
-  Deprecation.warning "Mocha::Standalone has been renamed to Mocha::API"
-  return API
-end
