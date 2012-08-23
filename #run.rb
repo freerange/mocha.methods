1ebbbe4934bf012e003fe1e1ddc186d1e7d7fc71
James Mead <james@floehopper.org>
2012-08-23 17:41:20 +0100

    Run new MiniTest & Test::Unit adapter tests in build matrix.
    Both in local build matrix script and (hopefully) in Travis CI.

--- #run.rb
+++ #run.rb
@@ -1,9 +1,9 @@
-def run(gemfile)
+def run(gemfile, task = "test")
   ENV["BUNDLE_GEMFILE"] = gemfile
   ENV["MOCHA_OPTIONS"] = "debug"
   reset_bundle
   execute(
     with_rbenv("bundle install --gemfile=#{gemfile}"),
-    with_rbenv("bundle exec rake test")
+    with_rbenv("bundle exec rake #{task}")
   )
 end
