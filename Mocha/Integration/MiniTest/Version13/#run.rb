06295c96f07e53f6c7b35a57859db79f7ba2ea14
James Mead <james@floehopper.org>
2012-08-23 16:51:25 +0100

    Rename MonkeyPatching back to Integration.
    This is to match up with the reverted commit [1] in master.

--- Mocha/Integration/MiniTest/Version13/#run.rb
+++ Mocha/Integration/MiniTest/Version13/#run.rb
@@ -10,12 +10,12 @@ def run runner
       @passed = true
     rescue Exception => e
       @passed = false
-      result = runner.puke(self.class, self.name, Mocha::MonkeyPatching::MiniTest.translate(e))
+      result = runner.puke(self.class, self.name, Mocha::Integration::MiniTest.translate(e))
     ensure
       begin
         self.teardown
       rescue Exception => e
-        result = runner.puke(self.class, self.name, Mocha::MonkeyPatching::MiniTest.translate(e))
+        result = runner.puke(self.class, self.name, Mocha::Integration::MiniTest.translate(e))
       end
     end
   ensure
--- Mocha/Integration/MiniTest/Version230To2101/#run.rb
+++ Mocha/Integration/MiniTest/Version230To2101/#run.rb
@@ -1,6 +1,6 @@
 def run runner
   trap 'INFO' do
-    time = Time.now - runner.start_time
+    time = runner.start_time ? Time.now - runner.start_time : 0
     warn "%s#%s %.2fs" % [self.class, self.__name__, time]
     runner.status $stderr
   end if ::MiniTest::Unit::TestCase::SUPPORTS_INFO_SIGNAL
@@ -11,6 +11,7 @@ def run runner
     begin
       @passed = nil
       self.setup
+      self.run_setup_hooks
       self.__send__ self.__name__
       mocha_verify(assertion_counter)
       result = "." unless io?
@@ -19,14 +20,15 @@ def run runner
       raise
     rescue Exception => e
       @passed = false
-      result = runner.puke self.class, self.__name__, Mocha::MonkeyPatching::MiniTest.translate(e)
+      result = runner.puke self.class, self.__name__, Mocha::Integration::MiniTest.translate(e)
     ensure
       begin
+        self.run_teardown_hooks
         self.teardown
       rescue *::MiniTest::Unit::TestCase::PASSTHROUGH_EXCEPTIONS
         raise
       rescue Exception => e
-        result = runner.puke self.class, self.__name__, Mocha::MonkeyPatching::MiniTest.translate(e)
+        result = runner.puke self.class, self.__name__, Mocha::Integration::MiniTest.translate(e)
       end
       trap 'INFO', 'DEFAULT' if ::MiniTest::Unit::TestCase::SUPPORTS_INFO_SIGNAL
     end
