8e6f4d886376cb876fe5f344cba2eb9aa0623fca
James Mead <james@floehopper.org>
2012-08-24 11:14:26 +0100

    Move MiniTest monkey-patching code into the individual version modules.
    By doing this I hope to make it easier to do something different for the
    most recent versions where we want to use the new adapter.
    
    This commit does introduce significant duplication, but I hope to remove
    that later.

--- Mocha/Integration/MiniTest/Version201To222/.included.rb
+++ Mocha/Integration/MiniTest/Version201To222/.included.rb
@@ -1,3 +1,11 @@
 def self.included(mod)
   $stderr.puts "Monkey patching MiniTest >= v2.0.1 <= v2.2.2" if $mocha_options['debug']
+  unless mod.ancestors.include?(Mocha::API)
+    mod.send(:include, Mocha::API)
+  end
+  unless mod.method_defined?(:run_before_mocha)
+    mod.send(:alias_method, :run_before_mocha, :run)
+    mod.send(:remove_method, :run)
+    mod.send(:include, InstanceMethods)
+  end
 end
