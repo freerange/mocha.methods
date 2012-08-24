72a5067915c5203ee5de7b820eb52ec0a420c63a
James Mead <james@floehopper.org>
2012-08-24 13:36:21 +0100

    Move Test::Unit monkey-patching code into the individual version modules.
    By doing this I hope to make it easier to do something different for the
    most recent versions where we want to use the new adapter.
    
    This commit does introduce significant duplication, but I hope to remove
    that later. And encouragingly the duplicate code is now almost identical
    to the MiniTest equivalent - see
    7ed2733b7da21d62e34ae6bf6209861877adb76b.

--- Mocha/Integration/TestUnit/GemVersion200/.included.rb
+++ Mocha/Integration/TestUnit/GemVersion200/.included.rb
@@ -1,3 +1,11 @@
 def self.included(mod)
   $stderr.puts "Monkey patching Test::Unit gem v2.0.0" if $mocha_options['debug']
+  unless mod.ancestors.include?(Mocha::API)
+    mod.send(:include, Mocha::API)
+  end
+  unless mod.method_defined?(:run_before_mocha)
+    mod.send(:alias_method, :run_before_mocha, :run)
+    mod.send(:remove_method, :run)
+    mod.send(:include, InstanceMethods)
+  end
 end
