c120a32698861ef62d13d2d9f1590cceb751cc68
James Mead <james@floehopper.org>
2012-10-27 18:02:58 +0100

    Make deprecation warnings more prominent.
    I'm about to issue some important deprecation warnings, so I want as
    many people as possible to notice them.

--- Mocha/Deprecation/.warning.rb
+++ Mocha/Deprecation/.warning.rb
@@ -1,5 +1,5 @@
 def warning(message)
   @messages << message
-  $stderr.puts "Mocha deprecation warning: #{message}" unless mode == :disabled
+  $stderr.puts "\n*** Mocha deprecation warning: #{message}\n\n" unless mode == :disabled
   $stderr.puts caller.join("\n  ") if mode == :debug
 end
