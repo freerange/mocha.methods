a5d38d7d5d1ccc2b9f637b68a40f8ab0917e53ba
Neal Gokli <neal.gokli@gmail.com>
2012-11-15 11:13:29 -0800

    Untested fix for #97: has_entry() does not work with an array as a hash value. Explicitly populate the key and value in different cases, rather than using flatten. Untested because I use rvm, which interferes with mocha unit testing

--- Mocha/ParameterMatchers/#has_entry.rb
+++ Mocha/ParameterMatchers/#has_entry.rb
@@ -1,14 +1,20 @@
 def has_entry(*options)
-  key, value = options.shift, options.shift
-  if key.is_a?(Hash)
-    case key.length
+  param_1 = options.shift
+  if options.empty? && param_1.is_a?(Hash)
+    case param_1.length
     when 0
       raise ArgumentError.new("Argument has no entries.")
     when 1
-      key, value = key.to_a.flatten
+      key   = param_1.keys.first
+      value = param_1[key]
     else
       raise ArgumentError.new("Argument has multiple entries. Use Mocha::ParameterMatchers#has_entries instead.")
     end
+  elsif options.length == 1
+    key   = param_1
+    value = options.shift
+  else
+    raise ArgumentError.new("Wrong number of arguments.  has_entry takes either one parameter (must be a hash) or two parameters (any key and value).")
   end
   HasEntry.new(key, value)
 end
