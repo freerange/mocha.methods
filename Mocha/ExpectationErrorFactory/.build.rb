c47f4d73f05c4af66809df9470f27a7fd80424f8
James Mead <james@floehopper.org>
2013-01-03 17:57:38 +0000

    Make unit tests more robust to changes in environment. Fixes #121.
    @voxik wanted to run the tests by simply requiring the files rather than
    running the rake task. If the unit tests and acceptance tests are loaded
    in the same process and if a recent version of MiniTest is available,
    then `ExpectationErrorFactory.exception_class` was being set to
    `MiniTest::Assertion` at file load time and then later when the unit
    tests ran, the latter exception was being raised instead of the default
    `ExpectationError` exception. This causes a number of unit tests to
    fail.
    
    I have fixed this by changing the assertions to expect
    `ExpectationErrorFactory.exception_class`. I've also had to change
    `ExpectationErrorFactory.exception_class` to be set to the default at
    file load time to make this work.

--- Mocha/ExpectationErrorFactory/.build.rb
+++ Mocha/ExpectationErrorFactory/.build.rb
@@ -1,5 +1,4 @@
 def build(message = nil, backtrace = [])
-  self.exception_class ||= ExpectationError
   exception = exception_class.new(message)
   filter = BacktraceFilter.new
   exception.set_backtrace(filter.filtered(backtrace))
