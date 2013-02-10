62b8485e18f6a3a8271d1600011db4ea2935ec78
James Mead <james@floehopper.org>
2013-02-10 22:16:19 +0000

    Allow stubbing of private Kernel methods. Fixes #134.
    We were only testing stubbing of the public and protected methods on
    Object.
    
    Note that `#method_defined?` and `#instance_methods` include both public
    and protected, but not private methods.
    
    There are still a few methods that are awkward, but I've just
    excluded them from the tests for now.
    
    There's probably a better solution to this in 1.9 involving inheriting
    Mock from BasicObject instead of Object, but that's a job for another
    day.

--- Mocha/Mock/#ensure_method_not_already_defined.rb
+++ Mocha/Mock/#ensure_method_not_already_defined.rb
@@ -1,3 +1,3 @@
 def ensure_method_not_already_defined(method_name)
-  self.__metaclass__.send(:undef_method, method_name) if self.__metaclass__.method_defined?(method_name)
+  self.__metaclass__.send(:undef_method, method_name) if self.__metaclass__.method_defined?(method_name) || self.__metaclass__.private_method_defined?(method_name)
 end
