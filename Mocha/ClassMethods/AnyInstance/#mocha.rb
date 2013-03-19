ea918a279113fa28e952fe06068976d00d6ff886
James Mead <james@floehopper.org>
2013-03-19 16:26:44 +0000

    Revert "Consider stubs on superclasses if none exist on receiver. Fixes #145."
    This reverts commit b41254ac391ce2274b89297b6bbb89b179a7df45.
    
    As pointed out by @ccutrer this solution doesn't work in the case where
    a method is stubbed on a child class and then another method is stubbed
    on the parent class. This is because as soon as the `@mocha` instance
    variable is set for the child class, it never tries to use the `@mocha`
    instance variable for the parent class. Time for a rethink and another
    look at #145.

--- Mocha/ClassMethods/AnyInstance/#mocha.rb
+++ Mocha/ClassMethods/AnyInstance/#mocha.rb
@@ -1,8 +1,3 @@
 def mocha
-  mocha, klass = nil, stubba_object
-  while mocha.nil? && klass.respond_to?(:superclass)
-    mocha = klass.any_instance.instance_variable_defined?(:@mocha) ? klass.any_instance.instance_variable_get(:@mocha) : nil
-    klass = klass.superclass
-  end
-  mocha || @mocha = Mocha::Mockery.instance.mock_impersonating_any_instance_of(@stubba_object)
+  @mocha ||= Mocha::Mockery.instance.mock_impersonating_any_instance_of(@stubba_object)
 end
