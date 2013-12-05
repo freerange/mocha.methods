68f9f0f5966db0220b62efb38d52899cf2b83a66
Arthur Neves <arthurnn@gmail.com>
2013-12-05 13:10:58 -0500

    Make class_eval line number more accurate.
    This sets the line number as the line number of the `def` statement.

--- Mocha/AnyInstanceMethod/#define_new_method.rb
+++ Mocha/AnyInstanceMethod/#define_new_method.rb
@@ -1,7 +1,7 @@
 def define_new_method
-  stubbee.class_eval(%{
+  stubbee.class_eval(<<-CODE, __FILE__, __LINE__ + 1)
     def #{method}(*args, &block)
       self.class.any_instance.mocha.method_missing(:#{method}, *args, &block)
     end
-  }, __FILE__, __LINE__)
+  CODE
 end
